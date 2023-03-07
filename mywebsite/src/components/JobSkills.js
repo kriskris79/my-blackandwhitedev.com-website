2ee6599105defd666b47da66ad04e3f5c42584d6
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-03-07 23:37:43 +0000

    more jobskills

--- mywebsite/src/components/JobSkills.js
+++ mywebsite/src/components/JobSkills.js
@@ -6,50 +6,57 @@ function JobSkills () {
 
 
 
-<div className="cards">
+        <div className="cards">
             <div className="panel">
-        <div className="ring">
-            <div className="card card1"></div>
-            <div className="border">
-                <p className="title">JS</p>
-                <div className="slide">
-                    <h6 className="para">JavaScript</h6>
+                <div className="ring">
+                    <div className="card card1"></div>
+                    <div className="border">
+                        <p className="title">JS</p>
+                        <p1 className="title">HTML</p1>
+                        <div className="slide">
+                            <h5 className="para">JavaScript</h5>
+                            <h6 className="para">HTML</h6>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className="panel">
-        <div className="ring">
-            <div className="card card2"></div>
-            <div className="border">
-                <p className="title">CSS</p>
-                <div className="slide">
-                    <h6 className="para">CSS </h6>
-                    <h6 className="para">Bootstrap  </h6>
+            <div className="panel">
+                <div className="ring">
+                    <div className="card card2"></div>
+                    <div className="border">
+                        <p className="title">CSS</p>
+                        <p1 className="title">SCSS</p1>
+                        <div className="slide">
+                            <h5 className="para">CSS </h5>
+                            <h6 className="para">SCSS  </h6>
 
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className="panel">
-        <div className="ring">
-            <div className="card card3"></div>
-            <div className="border">
-                <p className="title">HTML</p>
-                <div className="slide">
-                    <h6 className="para">HTML</h6>
+            <div className="panel">
+                <div className="ring">
+                    <div className="card card3"></div>
+                    <div className="border">
+                        <p className="title">Git</p>
+                        <p1 className="title">Scrum</p1>
+                        <div className="slide">
+                            <h5 className="para">Git</h5>
+                            <h6 className="para">Scrum</h6>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
             <div className="panel">
                 <div className="ring">
                     <div className="card card4"></div>
                     <div className="border">
                         <p className="title">React</p>
+                        <p1 className="title">WordPress</p1>
                         <div className="slide">
-                            <h6 className="para">React</h6>
+                            <h5 className="para">React</h5>
+                            <h6 className="para">WordPress</h6>
                         </div>
                     </div>
                 </div>
@@ -60,8 +67,10 @@ function JobSkills () {
                     <div className="card card5"></div>
                     <div className="border">
                         <p className="title">Photoshop</p>
+                        <p1 className="title">Adobe XD</p1>
                         <div className="slide">
-                            <h6 className="para">Photoshop</h6>
+                            <h5 className="para">Photoshop</h5>
+                            <h6 className="para">Adobe XD</h6>
                         </div>
                     </div>
                 </div>
@@ -69,8 +78,9 @@ function JobSkills () {
 
 
 
-</div>
+        </div>
     );
 }
 
 export default JobSkills;
+
